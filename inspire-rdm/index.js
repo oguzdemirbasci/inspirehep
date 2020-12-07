60a18a591600e3029856f82d79a3ef4ac7d0cb40
MJedr <jedrych.marcysia@gmail.com>
2020-12-07 13:05:46 +0100

    graphql: add conference schema

--- inspire-rdm/index.js
+++ inspire-rdm/index.js
@@ -1,15 +1,20 @@
 const { ApolloServer } = require('apollo-server');
- 
-const typeDefs = require("./schema");
-const resolvers = require("./resolvers");
+
+const { conferencesSchema } = require('./schemas/conferences');
+const resolvers = require('./resolvers');
 const { ConferenceDataSource } = require('./inspire-data-source');
 
-const server = new ApolloServer({ typeDefs, resolvers, dataSources: () => {
+const typeDefs = conferencesSchema;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
     return {
-        conferences : new ConferenceDataSource()
-    }
-} });
+      conferences: new ConferenceDataSource(),
+    };
+  },
+});
 
 server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
+  console.log(`🚀 Server ready at ${url}`);
 });
